8ef626e04273f3e48fdeb33bc2d2ba607ceb5e9a
Alan <ahw9f@virginia.edu>
2022-07-20 09:26:15 -0400

    for publish?

--- docs/functions/api.js
+++ docs/functions/api.js
@@ -1,5 +1,5 @@
 'use strict'
-const DataHandler = require('../data_handler.min.js'),
+const DataHandler = require('../data_handler.v1.min.js'),
   data = new DataHandler(require('../settings.json'), void 0, {
     data: require('../data.json'),
   })
